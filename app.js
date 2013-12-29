b4d66a5bc3bfaaa5a044a2434cd2ee60eacd5d30
Colin Mackey <colin@fishbrains.com>
2013-12-29 13:02:24 -0500

    config and server connect

--- app.js
+++ app.js
@@ -2,3 +2,24 @@ var application_root = _dirname,
 	express = require("express"),
 	path = require("path"),
 	mongoose = require('mongoose');
+
+var app = express.createServer();
+
+// Database
+mongoose.connect('mongodc://localhost/ecomm_database');
+
+// Config
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(application_root, "public")));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true}));
+});
+
+app.get('/api', function(req, res) {
+	res.send('Ecomm API is running');
+});
+
+// Launch server
+app.listen(4242);
